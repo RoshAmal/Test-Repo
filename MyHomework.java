8251608bcda30b14bd58b7c78ae166a7ce0a74ff
Amal Rosh <amalrosh707@gmail.com>
2021-09-27 20:47:03 +0530

    initial commit

---
+++ MyHomework.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class MyHomework {
+    public static void main(String args[]){
+
+    Scanner sc=new Scanner(System.in);
+    int radius=sc.nextInt();
+    float pi=(float) 3.14;
+    float area=pi*(float)radius*(float)radius;
+    System.out.println(area);
+
+    }
+}
