7c87142bfcfef7f35cb7e4186e57e2f2818668eb
Amal Rosh <amalrosh707@gmail.com>
2021-09-27 23:41:26 +0530

    Deleted import stmt

--- MyHomework.java
+++ MyHomework.java
@@ -1,10 +1,6 @@
-import java.util.*;
-
 public class MyHomework {
     public static void main(String args[]){
-
-    int i = 3;
-    int radius=i;
+    int radius=4;
     float pi=(float) 3.14;
     float area=pi*(float)radius*(float)radius;
     System.out.println(area);
