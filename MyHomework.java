feba217296479cf1716c430becf13919060e6979
Amal Rosh <amalrosh707@gmail.com>
2021-09-27 23:34:54 +0530

    changed mode of input

--- MyHomework.java
+++ MyHomework.java
@@ -3,8 +3,8 @@ import java.util.*;
 public class MyHomework {
     public static void main(String args[]){
 
-    Scanner sc=new Scanner(System.in);
-    int radius=sc.nextInt();
+    int i = 3;
+    int radius=i;
     float pi=(float) 3.14;
     float area=pi*(float)radius*(float)radius;
     System.out.println(area);
